0843afc7db882158a1f457327fe7c7a079344451
Fedor <109287919+End1m1one@users.noreply.github.com>
2022-10-31 09:05:24 +0300

    Delete Valid Parentheses.py

--- Valid Parentheses.py
+++
@@ -1,9 +0,0 @@
-s = '()'
-for char in range(0 ,len(s) ,2):
-    if s[char] == '(' and (s[char +1] != ')' or s[char +2] != ')'):
-        print(False)
-    if s[char] == '{' and (s[char +1] or s[char +2] != '}'):
-        print(False)
-    if s[char] == '[' and (s[char +1] or s[char +2] != ']'):
-        print(False)
-    else: print(True)
